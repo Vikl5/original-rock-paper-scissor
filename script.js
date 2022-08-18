0a234d2fcae48314f281665c250009021e4cf8aa
Vikl5 <Vikl5@protonmail.com>
2022-08-18 20:35:16 +0200

    Updated JS to use DOM-elements for output instead of the console

--- script.js
+++ script.js
@@ -6,46 +6,82 @@ const winCondition = {
 }
 const play = ["rock", "paper", "scissor"]
 const getComputerChoice = (choice) => choice[Math.floor(Math.random()*choice.length)]
-
-
+const results = document.querySelector("#results")
+const counter = document.querySelector("#counter")
+const container = document.querySelector("#container")
 let playerResult = 0
 let computerResult = 0
+let countRound = 0
 function playGame(playerSelect, computerSelect) {
+    results.textContent = `Round ${countRound + 1}: `;
     if (playerSelect === computerSelect) {
-        console.log("You chose:", playerSelect, "and the computer chose:",
-        computerSelect)
+        results.textContent += `You chose: ${playerSelect} and the computer chose:
+        ${computerSelect}`
     }
     else if (winCondition[playerSelect] === computerSelect) {
-        console.log("You chose:", playerSelect, "and the computer chose:",
-        computerSelect)
-        return playerResult += 1
+        playerResult++;
+        document.querySelector("#playerScore").textContent = playerResult
+        results.textContent += `You chose: ${playerSelect} and the computer chose:
+        ${computerSelect}`
     }
     else {
-        console.log("You chose:", playerSelect, "and the computer chose:",
-        computerSelect)
-        return computerResult += 1
+        computerResult++;
+        document.querySelector("#computerScore").textContent = computerResult
+        results.textContent += `You chose: ${playerSelect} and the computer chose:
+        ${computerSelect}`
     }
+    countRound++;
 }
 
 
-function getGame() {
-    for (let i = 0; i < 5; i++) {
-        const playSelect = prompt("Make your choice")
-        const compSelect = getComputerChoice(play)
-        playGame(playSelect, compSelect)
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorButton = document.querySelector("#scissor");
+let rounds = 0
+rockButton.addEventListener("click", () => {
+    const compSelect = getComputerChoice(play);
+    const playRock = "rock";
+    playGame(playRock, compSelect);
+    rounds += 1
+    isGameDone();
+});
+
+paperButton.addEventListener("click", () => {
+    const compSelect = getComputerChoice(play);
+    const playPaper = "paper";
+    playGame(playPaper, compSelect);
+    rounds += 1
+    isGameDone();
+});
+
+scissorButton.addEventListener("click", () => {
+    const compSelect = getComputerChoice(play);
+    const playScissor = "scissor";
+    playGame(playScissor, compSelect);
+    rounds += 1
+    isGameDone();
+});
+
+
+function isGameDone() {
+    if (rounds === 5) {
+        getResult();
+        container.classList.add("inactive")
+        setTimeout(getResult, 2000);
     }
+    else return;
 }
-getGame()
+
 
 function getResult() {
     if (playerResult === computerResult) {
-        console.log("It's a tie")
+        results.textContent = "It's a tie! You had a score of " + playerResult + " and the computer had a score of " + computerResult;
     }
     else if (playerResult < computerResult) {
-        console.log("Computer wins")
+        results.textContent = "The computer wins! You had a score of " + playerResult + " and the computer had a score of " + computerResult;
     }
     else {
-        console.log("You win")
+        results.textContent = "You win! You had a score of " + playerResult + " and the computer had a score of " + computerResult;
+    
     }
 }
-getResult()
